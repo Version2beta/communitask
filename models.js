71705b4702c11f3d6fd0d15c07c1cfbf9de1a958
Rob Martin <rob@version2beta.com>
2013-08-18 23:00:52 +0000

    First whack at the cradle

--- models.js
+++ models.js
@@ -1,91 +1,34 @@
-var Backbone = require('backbone');
 var _ = require('underscore')._;
+var config = require('./config');
+
+var cradle = require('cradle');
+var con = new(cradle.Connection)(
+    'localhost',
+    5984,
+    {
+      auth: {
+        username: 'communitask',
+        password: 'drunken%20himself%20holland'
+      },
+      retries: 3,
+      retryTimeout: 30 * 1000
+    });
+
+var createIfNeeded = function (db) {
+  db.exists(function (err, exists) {
+    if (err) {
+      console.log('error', err);
+      process.exit(1);
+    } else if (!exists) {
+      console.log('database does not exists.');
+      console.log(db.info());
+      db.create();
+      /* populate design documents */
+    }
+  });
+}
+
+dbCommunities = con.database('communities');
+createIfNeeded(dbCommunities);
 
 var models = module.exports = {};
-
-models.Community = Backbone.Model.extend({
-  initialize: function (opts) {
-    this.id = opts.id;
-    this.name = opts.name;
-    this.avatar = opts.avatar;
-    this.link = opts.link;
-    this.owners = opts.owners;
-    this.status = opts.status || 'created';
-  },
-  url: function() { return '/communities/' + this.id; },
-  collection: models.Communities
-});
-
-models.Communities = Backbone.Collection.extend({
-  model: models.Community,
-  url: '/communities'
-});
-
-models.Member = Backbone.Model.extend({
-  initialize: function (opts) {
-    this.id = opts.id;
-    this.name = opts.name;
-    this.email = opts.email;
-    this.hash = opts.hash;
-    this.phone = opts.phone;
-    this.communities = opts.communities;
-    this.avatar = opts.avatar;
-    this.status = opts.status || 'invited';
-  },
-  url: function() { return '/members/' + this.id; },
-  collection: models.Members
-});
-
-models.Members = Backbone.Collection.extend({
-  model: models.Member
-});
-
-models.Task = Backbone.Model.extend({
-  initialize: function (opts) {
-    this.id = opts.id;
-    this.title = opts.title;
-    this.description = opts.description;
-    this.community = opts.community;
-    this.categories = opts.categories;
-    this.creator = opts.creator;
-    this.createdOn = new Date();
-    this.status = opts.status || 'created';
-    this.owner = opts.owner;
-    this.due = opts.due;
-    this.priority = opts.priority || 0;
-    this.done = null;
-    this.doneBy = null;
-    this.value = opts.value || 0;
-  },
-  url: function() { return '/tasks/' + this.id; },
-  collection: models.Tasks
-});
-
-models.Tasks = Backbone.Collection.extend({
-  model: models.Task
-});
-
-models.RecurringTask = Backbone.Model.extend({
-  initialize: function (opts) {
-    this.id = opts.id;
-    this.title = opts.title;
-    this.description = opts.description;
-    this.link = opts.link;
-    this.community = opts.community;
-    this.categories = opts.categories;
-    this.createdOn = new Date();
-    this.status = opts.status || 'active';
-    this.owner = opts.owner;
-    this.assignees = opts.assignees;
-    this.defaultPriority = opts.defaultPriority || 0;
-    this.schedule = opts.schedule;
-    this.window = opts.window;
-    this.defaultValue = opts.defaultValue;
-  },
-  url: function() { return '/recurringtasks/' + this.id; },
-  collection: models.RecurringTasks
-});
-
-models.RecurringTasks = Backbone.Collection.extend({
-  model: models.RecurringTask
-});
